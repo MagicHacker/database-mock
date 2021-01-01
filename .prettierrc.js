f390c17a5f7a54578f389e9bfeb3b9a95cdb75fa
Johnson <zxmagichacker@gmail.com>
2021-01-01 23:05:47 +0800

    feat: 添加prettier配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: true,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+}
